2b1061e8e422f6707874d223351d9d21bbc435a4
vijeyash1 <vijeyash@gmail.com>
2022-12-07 23:26:06 +0530

    with gracefull shutdown

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import (
 	"api/handlers"
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,5 +19,25 @@ func main() {
 	r := gin.Default()
 	r.POST("/add", h.AddPolicyHandler)
 	r.POST("/check", h.CheckPolicyHandler)
-	r.Run()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
